2f1eb12f4f5ad3a6c771d9c42ec04d6aaca6a746
rETelect <157603862+rETelect@users.noreply.github.com>
2025-12-24 15:00:29 +0300

    Create mario.c

---
+++ Week 1 /mario.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (int row = 0; row < height; row++)
+    {
+
+        for (int space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+
+        for (int hash = 0; hash <= row; hash++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int hash = 0; hash <= row; hash++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}

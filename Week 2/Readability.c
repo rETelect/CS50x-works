844fa07aba799634355c99aacfdae8a244f51c8c
rETelect <157603862+rETelect@users.noreply.github.com>
2025-12-24 15:22:45 +0300

    Create readability.c

---
+++ Week 2/Readability.c
@@ -0,0 +1,61 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+
+    string text = get_string("Text: ");
+
+
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+
+        else if (isspace(text[i]))
+        {
+            words++;
+        }
+
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+
+
+    float calculation = 0.0588 * L - 0.296 * S - 15.8;
+
+
+    int index = round(calculation);
+
+    
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
